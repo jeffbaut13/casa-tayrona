d8219c070df17c8fae5651bff5d957b14eb6bfa6
jeffbaut13 <jeferssonabv@gmail.com>
2024-07-25 15:10:17 -0500

    loading terminado

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,23 @@ import "./index.css"; // Asegúrate de importar Tailwind CSS
 import videoCasa from "./assets/playa.mp4";
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
+  const [play, setPlay] = useState(false);
   const [started, setStarted] = useState(false);
 
-  const handleStart = () => {
-    setStarted(true);
-  };
-
-  useEffect(() => {
-    // Simulate video preloading
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  }, []);
-
-  if (loading) {
-    return <Loader setLoading={setLoading} />;
-  }
-
-  if (!started) {
-    return <StartButton onClick={handleStart} />;
-  }
+  console.log("el video esta en play? " + play);
+  console.log("el video esta " + started);
 
   return (
-    <VideoPlayer
-      videoSrc={videoCasa}
-      setStarted={setStarted}
-      started={started}
-    />
+    <>
+      <Loader setPlay={setPlay} started={started} />
+
+      <VideoPlayer
+        videoSrc={videoCasa}
+        setStarted={setStarted}
+        started={started}
+        play={play}
+      />
+    </>
   );
 };
 
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,11 +1,10 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { gsap } from 'gsap';
+import React, { useRef, useEffect, useState } from "react";
+import { gsap } from "gsap";
 
-const VideoPlayer = ({ videoSrc, setStarted, started }) => {
+const VideoPlayer = ({ videoSrc, setStarted, started, play }) => {
   const videoRef = useRef(null);
   const terrazaButtonRef = useRef(null);
   const habitacionButtonRef = useRef(null);
-  const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
     const video = videoRef.current;
@@ -14,54 +13,60 @@ const VideoPlayer = ({ videoSrc, setStarted, started }) => {
       setStarted(true);
     };
 
-    video.addEventListener('loadeddata', handleLoadedData);
+    video.addEventListener("loadeddata", handleLoadedData);
 
     return () => {
-      video.removeEventListener('loadeddata', handleLoadedData);
+      video.removeEventListener("loadeddata", handleLoadedData);
     };
   }, [setStarted]);
 
   useEffect(() => {
-    if (isPlaying) {
-      const video = videoRef.current;
+    const video = videoRef.current;
 
-      const showButton = (buttonRef, start, end) => {
-        gsap.to(buttonRef.current, {
-          opacity: 1,
-          duration: 0.5,
-          delay: start,
-          onComplete: () => {
-            gsap.to(buttonRef.current, {
-              opacity: 0,
-              duration: 0.5,
-              delay: end - start,
-            });
-          },
-        });
-      };
-
-      video.addEventListener('timeupdate', () => {
-        const currentTime = video.currentTime;
-        
-        if (currentTime >= 0 && currentTime < 6) {
-          showButton(terrazaButtonRef, 0, 6);
-        }
-        
-        if (currentTime >= 8 && currentTime < 15) {
-          showButton(habitacionButtonRef, 0, 7);
-        }
+    const showButton = (buttonRef, start, end) => {
+      gsap.to(buttonRef.current, {
+        opacity: 1,
+        duration: 0.5,
+        delay: start,
+        onComplete: () => {
+          gsap.to(buttonRef.current, {
+            opacity: 0,
+            duration: 0.5,
+            delay: end - start,
+          });
+        },
       });
-    }
-  }, [isPlaying]);
+    };
 
-  const handlePlay = () => {
-    setIsPlaying(true);
-    videoRef.current.play();
-  };
+    video.addEventListener("timeupdate", () => {
+      const currentTime = video.currentTime;
+
+      if (currentTime >= 1 && currentTime < 6) {
+        showButton(terrazaButtonRef, 0, 6);
+      }
+
+      if (currentTime >= 8 && currentTime < 15) {
+        showButton(habitacionButtonRef, 0, 7);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    if (play) {
+      videoRef.current.play();
+    }
+  }, [play]);
 
   return (
-    <div className="relative">
-      <video ref={videoRef} src={videoSrc} className="w-full" loop muted autoPlay playsInline />
+    <div className="absolute z-0 top-0 left-0 w-full h-full overflow-hidden">
+      <video
+        ref={videoRef}
+        src={videoSrc}
+        className="w-full"
+        loop
+        muted
+        playsInline
+      />
 
       {started && (
         <div className="absolute top-0 left-0 w-full h-full pointer-events-none">
@@ -77,14 +82,6 @@ const VideoPlayer = ({ videoSrc, setStarted, started }) => {
             className="absolute left-10 top-20 opacity-0 bg-red-500 text-white px-4 py-2 rounded pointer-events-auto"
           >
             Habitación
-          </button>
-        </div>
-      )}
-
-      {!isPlaying && (
-        <div className="absolute inset-0 flex justify-center items-center">
-          <button onClick={handlePlay} className="bg-blue-500 text-white px-4 py-2 rounded">
-            Reproducir
           </button>
         </div>
       )}
