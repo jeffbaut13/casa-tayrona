a10aaa1eafd1444b7edcdf56b3301be4eea5fa0f
ChristianFajardo2022 <chfajard@gmail.com>
2024-07-30 15:54:00 -0500

    audio

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -65,44 +65,50 @@ export const Loader = ({ setPlay, started, setloading, handleClickAudio }) => {
 
   return (
     <div
-      ref={container}
-      className="w-full h-full flex flex-col justify-evenly items-center absolute top-0 left-0 z-10"
+  ref={container}
+  className="w-full h-full flex flex-col justify-between items-center absolute top-0 left-0 z-10"
+>
+  <div
+    className="background pointer-events-none fixed top-0 left-0 w-screen h-screen z-0"
+    style={{ backgroundImage: 'url(/imagenes-tarjetas/bg.jpg)', backgroundSize: 'cover', backgroundPosition: 'center' }}
+  />
+  <div className="mt-8">
+    <Lottie loop={false} className="LoadIcon z-20" animationData={data} />
+  </div>
+  <div className="flex justify-center w-full mb-8">
+    <button
+      onClick={started ? handleLoading : null}
+      className="btnInicio flex justify-center items-center blackerMedium z-20 text-[--bg] border-2 border-[--bg] py-1 px-16 mb-32 text-xl"
     >
-      <div className="background pointer-events-none fixed top-0 left-0 bg-[--bg] w-screen h-screen z-10" />
-      <div />
-      <Lottie loop={false} className="LoadIcon  z-20" animationData={data} />
+      {!started ? (
+        <figure className="inline-block h-6 w-6">
+          <svg
+            className="animate-spin text-[--primary]"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </figure>
+      ) : (
+        "Empezar"
+      )}
+    </button>
+  </div>
+</div>
 
-      <button
-        onClick={started ? handleLoading : null}
-        className="btnInicio flex justify-center items-center blackerMedium z-20  text-[--primary] border-2 border-[--primary] py-1 px-16 text-xl"
-      >
-        {!started ? (
-          <figure className="inline-block h-6 w-6">
-            <svg
-              className="animate-spin text-[--primary]"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
-          </figure>
-        ) : (
-          "Empezar"
-        )}
-      </button>
-    </div>
   );
 };
